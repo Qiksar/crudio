96606f472748ec34995093b75725072c8d95c7a4
Qiksar Community <qiksar.apps@gmail.com>
2022-07-11 22:09:46 +0800

    Bulkload with foreign keys

--- src/CrudioDataWrapper.ts
+++ src/CrudioDataWrapper.ts
@@ -1,7 +1,7 @@
 import CrudioRepository from "./CrudioRepository";
 import CrudioField from "./CrudioField";
 import CrudioGQL from "./CrudioGQL";
-import CrudioTable from "./CrudioTable"
+import CrudioTable from "./CrudioTable";
 import { ICrudioConfig } from "./CrudioTypes";
 
 export default class CrudioDataWrapper {
@@ -18,81 +18,97 @@ export default class CrudioDataWrapper {
   public async DropTables() {
     const list = this.repo.tables;
     for (var index = 0; index < list.length; index++) {
-      const t: CrudioTable = list[index];
-      const sql = `DROP TABLE IF EXISTS "${this.config.schema}"."${t.name}" CASCADE;`;
+      var t: CrudioTable = list[index];
+      var sql = `DROP TABLE IF EXISTS "${this.config.schema}"."${t.name}" CASCADE;`;
       await this.gql.ExecuteSQL(sql);
     }
   }
 
   public async CreateTables() {
     const tables = this.repo.tables;
-    
+
     for (var index = 0; index < tables.length; index++) {
       const table: CrudioTable = tables[index];
       const entity = this.repo.GetEntityDefinition(table.entity);
 
       const key: CrudioField = entity.GetKey();
-      const keyField = `${key.fieldName} uuid DEFAULT gen_random_uuid() PRIMARY KEY`;
+      const keyField = `"${key.fieldName}" uuid DEFAULT gen_random_uuid() PRIMARY KEY`;
 
       var sql_fields_definitions = "";
-
-      const insert_fieldnames = [];
       var sql_column_names = `"${key.fieldName}"`;
-      insert_fieldnames.push(key.fieldName);
+      const insert_fieldnames = [key.fieldName];
+
 
       entity.fields.map((f: CrudioField) => {
         if (f.fieldName != key.fieldName) {
-          sql_fields_definitions += `, ${f.fieldName} ${f.GetDatabaseFieldType} `;
+          sql_fields_definitions += `, "${f.fieldName}" ${f.GetDatabaseFieldType} `;
 
-          if (f.defaultValue) sql_fields_definitions += `DEFAULT "${f.defaultValue} "`;
+          if (f.defaultValue)
+            sql_fields_definitions += `DEFAULT "${f.defaultValue} "`;
 
           sql_column_names += `, "${f.fieldName}"`;
           insert_fieldnames.push(f.fieldName);
         }
-
       });
 
-      entity.relationships.map(r=>{
+      // add foreign keys to insert columns
+      entity.relationships.map((r) => {
         sql_column_names += `,"${r.FromColumn}"`;
         sql_fields_definitions += `, "${r.FromColumn}" uuid`;
 
         insert_fieldnames.push(r.FromColumn);
       });
 
-      const sql = `CREATE TABLE "${this.config.schema}"."${table.name}" (${keyField} ${sql_fields_definitions});`;
-      await this.gql.ExecuteSQL(sql);
+      // -------------- Build create table statement
 
-      var insert = `INSERT INTO "${this.config.schema}"."${table.name}" (${sql_column_names}) VALUES`;
+      const create_table = `CREATE TABLE "${this.config.schema}"."${table.name}" (${keyField} ${sql_fields_definitions});`;
+
+      var insert_rows = `INSERT INTO "${this.config.schema}"."${table.name}" (${sql_column_names}) VALUES`;
       const rows = table.rows;
 
-      for (var r = 0; r < 1/*rows.length*/; r++) {
+      // -------------- Build foreign keys
+
+      var create_foreign_keys = "";
+      entity.relationships.map((r) => {
+        const target = tables.filter((t) => t.entity === r.To)[0];
+
+        create_foreign_keys += `
+        ALTER TABLE "${this.config.schema}"."${table.name}"
+        ADD CONSTRAINT FK_${r.RelationshipName}
+        FOREIGN KEY("${r.FromColumn}") 
+        REFERENCES "${this.config.schema}"."${target.name}"("${r.ToColumn}");
+        `;
+      });
+
+      // -------------- Build insert rows
+
+      for (var r = 0; r < rows.length; r++) {
         const entity = rows[r];
         if (!entity) throw new Error("NULL data row");
 
         var values = "";
 
         insert_fieldnames.map((i) => {
-
           var v = entity.values[i];
 
-          if(v && v.values)
-          {
-            v=v.values.id;
+          if (v && v.values) {
+            v = v.values.id;
           }
 
-          values += `${
-            v ? "'" + v + "'" : "NULL"
-          },`;
+          values += `${v ? "'" + v + "'" : "NULL"},`;
         });
 
         values = values.substring(0, values.length - 1);
 
-        insert += `(${values}),`;
+        insert_rows += `(${values}),`;
       }
-      insert = insert.substring(0, insert.length - 1);
+      insert_rows = insert_rows.substring(0, insert_rows.length - 1);
 
-
-      await this.gql.ExecuteSQL(insert);
+      await this.gql.ExecuteSQL(create_table);
+      await this.gql.ExecuteSQL(insert_rows);
     }
+
+    if (create_foreign_keys.length > 0)
+      await this.gql.ExecuteSQL(create_foreign_keys);
   }
 }
