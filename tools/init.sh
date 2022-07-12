466c25e855084fdcd9fe75da6925e678460e7e85
Qiksar Community <qiksar.apps@gmail.com>
2022-07-12 13:33:57 +0800

    Improve git init

--- tools/init.sh
+++ tools/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir repo
-wget -O - -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/docker-compose.yml 
-wget -O - -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/repo/repo.json -P repo
-wget -O - -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/repo/standard_generators.json -P repo
+wget -q -O - -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/docker-compose.yml 
+wget -q -O - -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/repo/repo.json -P repo
+wget -q -O - -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/repo/standard_generators.json -P repo
 docker-compose up -d
 npx @qiksar/crudio repo/repo.json
