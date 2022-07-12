1b224e22cab64d46cdf7dc831c38443bcd20abde
Qiksar Community <qiksar.apps@gmail.com>
2022-07-12 13:41:20 +0800

    init

--- tools/init.sh
+++ tools/init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir repo
-wget -O - -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/repo/repo.json -P repo
-wget -O - -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/repo/standard_generators.json -P repo
-wget -O - -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/docker-compose.yml
+wget -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/repo/repo.json -P repo
+wget -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/repo/standard_generators.json -P repo
+wget -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/docker-compose.yml
 
 docker-compose up -d
 
