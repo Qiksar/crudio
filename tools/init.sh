cbfa815257827b3611c493602b9a91ad4ef07ca7
Qiksar Community <qiksar.apps@gmail.com>
2022-07-12 13:35:44 +0800

    quiet script

--- tools/init.sh
+++ tools/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir repo
-wget -q -O - -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/docker-compose.yml 
-wget -q -O - -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/repo/repo.json -P repo
-wget -q -O - -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/repo/standard_generators.json -P repo
+wget -O - -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/docker-compose.yml -q
+wget -O - -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/repo/repo.json -P repo -q
+wget -O - -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/qiksar/crudio/main/repo/standard_generators.json -P repo -q
 docker-compose up -d
 npx @qiksar/crudio repo/repo.json
