3e4bbdb52c1b91545d259ee70f46bd03a9f5ffd3
Qiksar Community <qiksar.apps@gmail.com>
2022-07-21 17:44:19 +0800

    fix

--- tools/init.sh
+++ tools/init.sh
@@ -3,6 +3,8 @@ wget -q -nc --no-check-certificate --content-disposition https://raw.githubuserc
 wget -q -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/Qiksar/crudio/main/repo/base_entity.json -P repo
 wget -q -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/Qiksar/crudio/main/repo/base_generators.json -P repo
 wget -q -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/Qiksar/crudio/main/repo/base_snippets.json -P repo
+wget -q -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/Qiksar/crudio/main/repo/iot.json -P repo
+wget -q -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/Qiksar/crudio/main/repo/repo.json -P repo
 wget -q -nc --no-check-certificate --content-disposition https://raw.githubusercontent.com/Qiksar/crudio/main/docker-compose.yml
 
 docker-compose up -d
