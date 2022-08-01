baa5621a485042dbb6897def8cdfd8080a27c9f0
Qiksar Community <qiksar.apps@gmail.com>
2022-08-01 14:05:29 +0800

    cleanup

--- test/unit/json.spec.ts
+++ test/unit/json.spec.ts
@@ -1,7 +1,7 @@
 import { CrudioJson } from "./../../src/CrudioJson";
 
 describe("JSON schema validation", () => {
-	test("Valid SubjectEncounter should comply with schema", () => {
+	test("Valid datamodel should comply with schema", () => {
 		const repo = CrudioJson.LoadJson("repo/repo.json");
 		CrudioJson.AddSchema("schema/crudio.json", "crudio");
 		const result = CrudioJson.Validate(repo, "crudio");
