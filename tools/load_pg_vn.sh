99cac1a3796a43892bbfd726481444af47822715
qiksar <qiksar+apps@gmail.com>
2023-04-08 09:05:28 +0800

    Fix file names

--- tools/load_pg_vn.sh
+++ tools/load_pg_vn.sh
@@ -1,9 +1,12 @@
 # Create the docker environment and load Postgres DB with test data
 #
+echo "DIRECTORY:"
+pwd
+echo
 echo "Vietnamese: Start docker, load PostgreSQL and automatically configure Hasura..."
 npm run docker_up 
 npm run build 
-node dist/cjs/cli/crudio_cli.js -w -v -m datamodel/datamodel.vn.json --target p -d test/output/datamodel.mermaid.md 
+node dist/cjs/cli/crudio_cli.js -w -v -m datamodel/datamodel_vn.json --target p -d test/output/datamodel.mermaid.md 
 
 echo ""
 echo "PostgreSQL database loaded"
